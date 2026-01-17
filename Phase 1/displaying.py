aa5bbaca0ba418ced66ed5dcd9114f3b7b896048
Rupesh <rupeshghimire377@gmail.com>
2026-01-17 21:03:06 +0545

    Displaying Image

--- Phase 1/displaying.py
+++ Phase 1/displaying.py
@@ -2,7 +2,9 @@ import cv2
 
 image = cv2.imread("school.jpg")
 
-if image is None:
-    print("Error: Image not found")
+if image is not None:
+    cv2.imshow("school picture", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 else:
-    print("Image loaded successfully")
+    print("could not load image")
