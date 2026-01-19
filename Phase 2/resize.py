7075e0e3200077dc33bbaf7cc1062adbc09b4447
Rupesh <rupeshghimire377@gmail.com>
2026-01-19 19:47:55 +0545

    phase 2

---
+++ Phase 2/resize.py
@@ -0,0 +1,18 @@
+import cv2
+
+image = cv2.imread("school.jpg")
+
+if image is None:
+    print("Error loading image")
+else:
+    print("Image loaded successfully")
+
+    resized_image = cv2.resize(image, (500, 500))
+
+    cv2.imshow("Original Image", image)
+    cv2.imshow("Resized Image", resized_image)
+
+    cv2.imwrite("resized_school.jpg", resized_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
