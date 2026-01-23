db48d678bd0ab8f06251374c90eb58af44b41a67
Rupesh <rupeshghimire377@gmail.com>
2026-01-23 23:06:26 +0545

    recording video

---
+++ Phase 4/using_cap.py
@@ -0,0 +1,21 @@
+# using first function from the note
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read() #ret = True or False   frame = image
+
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    cv2.imshow("Webcam Feed", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Exiting...")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
