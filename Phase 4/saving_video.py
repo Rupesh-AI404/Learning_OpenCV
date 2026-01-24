235689823c9d6fdc6ad5e4b1b0e151306223f68d
Rupesh <rupeshghimire377@gmail.com>
2026-01-24 21:24:00 +0545

    saving recorded video

---
+++ Phase 4/saving_video.py
@@ -0,0 +1,27 @@
+from email.mime import image
+
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+codec = cv2.VideoWriter_fourcc(*'XVID')
+recorder = cv2.VideoWriter('my_video.avi', codec, 20.0, (frame_width, frame_height))
+
+while True:
+    success, image = camera.read()
+
+    if not success:
+        break
+
+    recorder.write(image)
+    cv2.imshow("Recording live video", image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+recorder.release()
+cv2.destroyAllWindows()
