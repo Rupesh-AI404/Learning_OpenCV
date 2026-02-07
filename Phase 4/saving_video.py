eae2c7409b926dd1c328b976011aad12fef1010a
Rupesh <rupeshghimire377@gmail.com>
2026-02-07 22:44:04 +0545

    update

--- Phase 4/saving_video.py
+++ Phase 4/saving_video.py
@@ -8,7 +8,7 @@ frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 codec = cv2.VideoWriter_fourcc(*'XVID')
-recorder = cv2.VideoWriter('my_video.avi', codec, 20.0, (frame_width, frame_height))
+recorder = cv2.VideoWriter('my_video.avi', codec, 10.0, (frame_width, frame_height))
 
 while True:
     success, image = camera.read()
