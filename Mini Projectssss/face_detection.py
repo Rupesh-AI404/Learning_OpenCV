a737e36badd7200256e2cd02d11fa2c0847f2ac0
Rupesh <rupeshghimire377@gmail.com>
2026-03-17 22:40:23 +0545

    face detection project final

--- Mini Projectssss/face_detection.py
+++ Mini Projectssss/face_detection.py
@@ -4,7 +4,7 @@ face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 )
 
-cap = cv2.VideoCapture(0)   # 0 = default webcam
+cap = cv2.VideoCapture(0)   #0 = default webcam
 
 while True:
     ret, frame = cap.read()
